1f39ab20d5f968c1527bda5519ac19949e9404ac
Lachlan <42135069+lachlanelijah@users.noreply.github.com>
2023-05-03 11:44:23 +1000

    created initial project file

---
+++ TripHive/TripHive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripHive
+//
+//  Created by Lachlan Atack on 3/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
